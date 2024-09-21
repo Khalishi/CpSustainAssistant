7f9ccd1a3037a928dac10cefe3c4d678c46a6e12
Thanyani <khalishisimon@gmail.com>
2024-09-21 15:02:05 +0200

    modify vite styling

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,18 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
             ],
+            
             refresh: true,
         }),
     ],
+
+    server: {
+        // Match the host and port for Vite dev server
+        host: '67.207.82.128',  // Replace with your server's IP or domain
+        port: 5174,  // Ensure the port matches the one you're accessing
+        https: false,  // Disable HTTPS if your main app uses HTTP
+        cors: {
+            origin: ['http://67.207.82.128', 'https://cp-sustain-assistant.test'],
+        }
+    }
 });
